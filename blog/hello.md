0c813830fe325cd31773f1aa9939f692ea0f0560
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-09-10 16:02:32 +0700

    init ?

---
+++ blog/hello.md
@@ -0,0 +1,9 @@
+---
+slug: hello
+title: Hello
+author: Fiqry choerudin
+author_title: Frontend Developer
+author_url: https://github.com/fiqryq
+author_image_url: https://avatars.githubusercontent.com/u/25787603?v=4
+tags: [hello]
+---
---
+++ docusaurus.config.js
@@ -0,0 +1,76 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(
+  module.exports = {
+    title: "Suncode.id",
+    tagline: "Dinosaurs are cool",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "suncode", // Usually your GitHub org/user name.
+    projectName: "suncode", // Usually your repo name.
+
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          blog: {
+            showReadingTime: true,
+            routeBasePath: "/",
+            // Please change this to your repo.
+            editUrl:
+              "https://github.com/facebook/docusaurus/edit/main/website/blog/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        navbar: {
+          title: "Suncode",
+          logo: {
+            alt: "Suncode Logo",
+            src: "img/favicon.ico",
+          },
+          items: [
+            { to: "/", label: "Blog", position: "left" },
+            {
+              href: "https://github.com/facebook/docusaurus",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
+        },
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Github",
+              items: [
+                {
+                  label: "GitHub",
+                  href: "https://github.com/facebook/docusaurus",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Suncode.id`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+        },
+      }),
+  }
+);
